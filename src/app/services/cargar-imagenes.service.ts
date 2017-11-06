c3073d0501cf4c667577b9aed48801f32c67833b
yenni <7095>
2017-11-06 07:45:59 -0300

    cambio de envio de img

--- src/app/services/cargar-imagenes.service.ts
+++ src/app/services/cargar-imagenes.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { FileItem } from '../models/file-item';
 import { Http, Headers } from '@angular/http';
 import 'rxjs/Rx';
+import {Observable} from "rxjs";
+
 
 @Injectable()
 export class CargarImagenesService {
@@ -10,17 +12,52 @@ export class CargarImagenesService {
 
   constructor(private http: Http) { }
 
-  cargarDatosGaleria(titulo, descripcion, archivos: FileItem[]){
+  cargarDatosGaleria(titulo, descripcion, archivos:FileItem[]) {
     //this.cargar_imagenes(archivos);
-    let body = archivos;
-    let headers = new Headers({
-      'Content-Type':'application/json'
-    });
-    console.log(body);
-    return this.http.post( this.galeriaURl, body, { headers })
-      .map( res => {
-        return res.json();
+
+
+    return Observable.create(observer => {
+
+      let ListAr: File[]=[];
+      let formData=new FormData();
+      for(let nar of archivos){
+        ListAr.push(nar.archivo);
+        formData.append('file',nar.archivo);
+      }
+      let arc={file:ListAr[0]};
+
+
+      let body =formData;
+      console.log(body);
+
+      let xhr: XMLHttpRequest = new XMLHttpRequest();
+
+      xhr.addEventListener('readystatechange', function () {
+        if (this.readyState === 4) {
+          console.log(this.responseText);
+          return this.responseText;
+        }
       });
+
+
+
+      xhr.open('POST', this.galeriaURl, true);
+      xhr.setRequestHeader('enctype', 'multipart/form-data');
+      xhr.setRequestHeader('cache-control', 'no-cache');
+      xhr.send(formData);
+
+
+    });
+
+
+  /*  let headers = new Headers({
+      'enctype':'multipart/form-data',
+      'Content-Type':'multipart/form-data'
+    });
+    return this.http.post( this.galeriaURl, formData,{headers})
+      .map( res => {
+        return res;
+      }); */
   }
 
   cargar_imagenes( archivos: FileItem[] ) {
