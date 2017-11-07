e3931d1a7a825646cb6a60dc434c08db8e87cd47
Yennifer Pernia <perniayennifer@gmail.com>
2017-11-07 18:00:29 -0300

    Mostrar imagenes y eliminarlas

--- src/app/components/galeria/galeria.component.ts
+++ src/app/components/galeria/galeria.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { FileItem } from '../../models/file-item';
+import { GaleriaItems } from '../../Models/Galeria-items';
+import { Fotos } from '../../Models/Fotos';
 import { NgForm } from '@angular/forms';
 import { CargarImagenesService } from '../../services/cargar-imagenes.service';
+import {forEach} from '@angular/router/src/utils/collection';
 
 @Component({
   selector: 'app-galeria',
@@ -18,14 +21,21 @@ export class GaleriaComponent  {
 
   archivos: FileItem[] = [];
 
-  constructor(public _cargaImagenes: CargarImagenesService) { }
+  Imagenes: Fotos[]= [];
+  Galeria: GaleriaItems[]= [];
+
+  Modal:boolean = false;
+
+  constructor(public _cargaImagenes: CargarImagenesService) {
+    this.cargarGaleria();
+  }
 
   archivoSobreDropZone( e:boolean ){
     this.estaSobreDropZone = e;
   }
 
 
-  cargarImagenesGaleria(){
+  cargarImagenesGaleria() {
     this.permiteCargar = false;
     this._cargaImagenes.guardarImg( this.titulo, this.descripcion, this.archivos)
       .subscribe(data => {
@@ -33,11 +43,59 @@ export class GaleriaComponent  {
     });
   }
 
-
-  limpiarArchivos(){
+  limpiarArchivos() {
     this.archivos = [];
     this.permiteCargar = true;
   }
 
+  cargarGaleria() {
+    this.Galeria = [];
+    let galeria;
+    let fts;
+    this._cargaImagenes.ObtenerGaleria()
+      .subscribe(data => {
+        console.log(data);
+        for (let res of data){
+          this._cargaImagenes.cargar_imagenes('Galeria', res.id_g)
+            .subscribe(data1 => {
+              for (let res1 of data1) {
+                fts = new Fotos (res1.id_img, res1.url, res1.ref, res1.id_g, res1.id_m, res1.id_d);
+                this.Imagenes.push(fts);
+              }
+              galeria = new GaleriaItems(res.id_g, res.titulo, res.descripcion,  this.Imagenes);
+              this.Galeria.push(galeria);
+              this.Imagenes = [];
+            });
+        }
+
+        console.log(this.Galeria);
+
+      });
+  }
+
+  EliminarImg(id){
+    this._cargaImagenes.eliminar_imagen(id)
+      .subscribe(data =>{
+        console.log(data);
+        this.cargarGaleria();
+      });
+  }
+
+  /*for (let res of data){
+        this.Galeria.push(res.id_g, res.titulo, res.descripcion);
+         /*this._cargaImagenes.cargar_imagenes('Galeria', res.id)
+           .subscribe(data1 => {
+             for (let res1 of data1) {
+               this.Imagenes.push(res1.id_img, res1.url, res1.refc, res1.id_g, res1.id_m, res1.id_d);
+             }
+           });
+} */
+
+ /* cargarImsg(id) {
+    return this._cargaImagenes.cargar_imagenes('Galeria', '3')
+      .subscribe(data => {
+         this.Imagenes = data;
+      });
+  }*/
 
 }
